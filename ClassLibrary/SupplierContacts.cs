4ea035aed6c5d43a81990b3128b134fffe07c10e
Hoorasadat Mirsanei <mirsaanei@gmail.com>
2019-03-17 19:23:59 -0600

    Business Classes for Suppliers and SupplierContacts tables

---
+++ ClassLibrary/SupplierContacts.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_2
+{
+    /// <summary>
+    /// Hoora - March
+    /// Class to create suppliers objects
+    /// </summary>
+    public class SupplierContacts
+    {
+        // Constructor:
+        public SupplierContacts() { }
+
+        // properties:
+        public int SupplierContactId { get; set; }
+
+        public string SupConFirstName { get; set; }
+
+        public string SupConLastName { get; set; }
+
+        public string SupConCompany { get; set; }
+
+        public string SupConAddress { get; set; }
+
+        public string SupConCity { get; set; }
+
+        public string SupConProv { get; set; }
+
+        public string SupConPostal { get; set; }
+
+        public string SupConCountry { get; set; }
+
+        public string SupConBusPhone { get; set; }
+
+        public string SupConFax { get; set; }
+
+        public string SupConEmail { get; set; }
+
+        public string SupConURL { get; set; }
+
+        public string AffiliationID { get; set; }
+
+        public int? SupplierId { get; set; }
+
+        // methods:
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
--- ClassLibrary/Suppliers.cs
+++ ClassLibrary/Suppliers.cs
@@ -10,46 +10,24 @@ namespace Project_2
     /// Hoora - March
     /// Class to create suppliers objects
     /// </summary>
+    /// 
     public class Suppliers
     {
         // Constructor:
         public Suppliers() { }
 
+
         // properties:
-        public int SupplierContactId { get; set; }
+        public int SupplierId { get; set; }
 
-        public string SupConFirstName { get; set; }
+        public string SupName { get; set; }
 
-        public string SupConLastName { get; set; }
-
-        public string SupConCompany { get; set; }
-
-        public string SupConAddress { get; set; }
-
-        public string SupConCity { get; set; }
-
-        public string SupConProv { get; set; }
-
-        public string SupConPostal { get; set; }
-
-        public string SupConCountry { get; set; }
-
-        public string SupConBusPhone { get; set; }
-
-        public string SupConFax { get; set; }
-
-        public string SupConEmail { get; set; }
-
-        public string SupConURL { get; set; }
-
-        public string AffiliationID { get; set; }
-
-        public int? SupplierId { get; set; }
 
         // methods:
         public override string ToString()
         {
             return base.ToString();
         }
+
     }
 }
