a9163e42d156e44822a78f2d5789169c6f214a6e
Hoorasadat Mirsanei <mirsaanei@gmail.com>
2019-03-17 21:00:15 -0600

    adding AffiliationsDB class

---
+++ ClassDB/AffiliationsDB.cs
@@ -0,0 +1,77 @@
+﻿using Project_2;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDB
+{
+    /// <summary>
+    /// Hoora - March 
+    /// A data access class for dealing with suppliers info
+    /// </summary>
+    /// 
+    public static class AffiliationsDB
+    {
+        public static List<Affiliations> GetAff()
+        {
+            List<Affiliations> affiliations = new List<Affiliations>();
+            Affiliations afl;
+
+            SqlConnection cnc = TravelExpertDB.GetConnection();
+
+            string SelectQuery = "SELECT * FROM Affiliations";
+
+            SqlCommand cmnd = new SqlCommand(SelectQuery, cnc);
+
+            try
+            {
+                cnc.Open();
+
+                SqlDataReader dr = cmnd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    afl = new Affiliations();
+                    afl.AffiliationId =dr["AffiliationId"].ToString();
+
+
+                    int AflNameIndex = dr.GetOrdinal("AffName");
+                    if (dr.IsDBNull(AflNameIndex))
+                    {
+                        afl.AffName = null;
+                    }
+                    else
+                    {
+                        afl.AffName = dr["AffName"].ToString();
+                    }
+
+
+                    int AflDescIndex = dr.GetOrdinal("AffDesc");
+                    if (dr.IsDBNull(AflDescIndex))
+                    {
+                        afl.AffDesc = null;
+                    }
+                    else
+                    {
+                        afl.AffDesc = dr["AffDesc"].ToString();
+                    }
+
+                    affiliations.Add(afl);
+                }
+                dr.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                cnc.Close();
+            }
+            return affiliations;
+        }
+    }
+}
