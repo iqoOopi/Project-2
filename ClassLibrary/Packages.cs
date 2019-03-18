c33b8a659c085dd745c83e8940924c5add2a273d
MortyAllen <matthmac@gmail.com>
2019-03-18 09:29:19 -0600

    bug

---
+++ ClassLibrary/Packages.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+   public class Package
+    {
+        int PackageID { get; set; }
+
+        string PackageName { get; set; }
+
+        DateTime? PackageStart { get; set; }
+
+        DateTime? PackageEnd { get; set; }
+
+        string Desc { get; set; }
+
+        decimal BasePrice { get; set; }
+
+        decimal AgencyCom { get; set; }
+
+    }
+}
